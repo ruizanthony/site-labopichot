324daf3a720a34839707b8ebf87d98090ace42d2
anthony ruiz <ruizanthony@gmail.com>
2022-08-09 17:46:24 +0200

    test comit

---
+++ src/pages/CreationMoule.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+import {StaticImage} from "gatsby"
+
+const  CreationMoule = () => {
+    
+    // <StaticImage />
+    return (
+        <Layout>
+            <div>
+                Chez Laboratoires Pichot, nous partons toujours du vrai besoin client.
+
+                Nos clients créent des gammes de produits cohérentes, pertinentes et attirantes pour le consommateur.
+                Le désir d'acheter est souvent influencé par le packaging.
+
+                Ainsi, notre rôle est de vous accompagner dans la création de gammes de pack ajusté précisemment à vos besoins.
+                C'est cette fabrication de flacon à façon qui nous caractérise.
+
+                Nous fabriquons en France 100% de nos flacons plastiques dans notre site de production unique localisé dans le territoire du Livradois Forez, mais aussi nous concevons et fabriquons en interne nos moules.
+
+                C'est cette intégration en interne qui explique en partie notre grande réactivité. En moyenne, nous fabriquons un moule par semaine, et ce depuis 30 ans.
+                Ces moules sont a 95% notre pr
+
+            </div>
+        </Layout>
+    )
+}
